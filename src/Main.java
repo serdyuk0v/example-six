38e49403d863dc4418ebd2f1137b5949869397de
Serdyukov <and.serdykov@gmail.com>
2022-08-31 17:57:17 +0300

    Задача №2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = generateRandomArray();
+        int[] cash = generateRandomArray();
+        int minCashDay = 200000;
+        int maxCashDay = 100000;
 
-        public static int[] generateRandomArray() {
-            java.util.Random random = new java.util.Random();
-            int[] arr = new int[30];
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = random.nextInt(100_000) + 100_000;
+        for (int current : cash) {
+            if (current > maxCashDay) {
+                maxCashDay = current;
             }
-            return arr;
         }
+
+        for (int current : cash) {
+            if (current < minCashDay) {
+                minCashDay = current;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minCashDay + " рублей. Максимальная сумма трат за день составила " + maxCashDay + " рублей.");
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] cash = new int[30];
+        for (int i = 0; i < cash.length; i++) {
+            cash[i] = random.nextInt(100_000) + 100_000;
+        }
+        return cash;
     }
 }
