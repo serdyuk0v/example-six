8300bc9eb82a44e86d5e64a8245250fe5629c3b5
Serdyukov <and.serdykov@gmail.com>
2022-08-31 17:18:14 +0300

    Задача №1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int[] arr = generateRandomArray();
-
-        public static int[] generateRandomArray() {
-            java.util.Random random = new java.util.Random();
-            int[] arr = new int[30];
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = random.nextInt(100_000) + 100_000;
-            }
-            return arr;
+        int sum = 0;
+        for (int cash : arr) {
+            sum += cash;
         }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
