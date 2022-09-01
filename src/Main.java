2e90e74554b11b198880bfd209bb52409990863f
Serdyukov <and.serdykov@gmail.com>
2022-09-01 08:32:25 +0300

    Задача №4

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = generateRandomArray();
-
-        public static int[] generateRandomArray() {
-            java.util.Random random = new java.util.Random();
-            int[] arr = new int[30];
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = random.nextInt(100_000) + 100_000;
-            }
-            return arr;
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
     }
 }
